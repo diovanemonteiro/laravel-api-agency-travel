d67d8e20b8627df4d46d4d0c75c3a1b78a506277
Diovane <diovane.sousa@manaus.am.gov.br>
2023-07-03 10:42:52 -0400

    Ajusta a factory para gerar os tours

--- database/seeders/TravelSeeder.php
+++ database/seeders/TravelSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tour;
 use App\Models\Travel;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,6 +14,8 @@ class TravelSeeder extends Seeder
      */
     public function run(): void
     {
-        Travel::factory(50)->create();
+        Travel::factory(50)
+            ->has(Tour::factory()->count(10))
+            ->create();
     }
 }
